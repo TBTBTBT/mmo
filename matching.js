39e3a4c87b96bdecafeb654785c11789117db281
Shogo Tabata <>
2018-10-05 09:47:29 +0900

    シーケンス追加

--- matching.js
+++ matching.js
@@ -6,35 +6,61 @@ var WS_PORT = 3000;
 //client state list
 /*
 Connect
+Entry
 Wait
 Match
 Exit
 */
-var CF = class ClientFormat{
+class ClientFormat{
 	constructor(client,state){
 		this.client = client;
 		this.state = state;
 	}
 }
-var CM = class ClientManager extends Connection{
+class ClientManager extends Connection{
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//constructor
 	constructor(data){
 		super(data);
 		this.clients = {};
 		this.define();
 	}
-
-	//define
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//define
 	define(){
 		this.response = {
 			connect: this.resConnect
 		}
 	}
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//responce
 	resConnect(id,data){
 		var name = data.name;
+
 		console.log('client on stage named :' + name);
 	}
+	resEntry(id,data){
+		
+		this.clients[id].state = 'entry';
+	}
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//request
+
+	/*
+	reqRollCall(){
+		//closeでまかなえなかった場合に作成する
+		this.broadcastRollCall();
+	}*/
+	
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//callback
 	onOpen(id,client,req){
-		this.clients[id] = new CF(client,'connect');
+		this.clients[id] = new ClientFormat(client,'connect');
 		this.sendConnectionCallback(id,client);
 		console.log('client connected id:' + id);
 		console.log('clients length :' + Object.keys(this.clients).length);
@@ -55,6 +81,9 @@ var CM = class ClientManager extends Connection{
 		console.log(e);
 		
 	}
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//send
 	sendConnectionCallback(id,client){
 		var send = {};
 		send.type = 'connect';
@@ -62,30 +91,51 @@ var CM = class ClientManager extends Connection{
 		send.data.id = id;
 		super.send(client,JSON.stringify(send));
 	}
-	sendGameServerAddress(){
-		//roomId + address
-		var send = [];
-	}
-	broadcastEncount(name){
+	//omit
+	/*
+	broadcastRollCall(){
+
 		var send = {};
-		send.type = 'encount';
+		send.type = 'rollcall';
 		send.data = {};
 		send.data.id = id;
+	}*/
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+//update
+	updateClientState(){
+		this.clients.ForEach(
+			function each(c){
+				if(c.state == 'entry'){
+					c.state = 'wait';
+				}
+			});
+	}
+	update
+	updateMatching(){
+		this.clients.ForEach(
+			function each(c){
+				if(c.state == 'wait'){
+					
+				}
+			});
 	}
 }
-var MS = class MatchingServer{
+class MatchingServer{
 	constructor(data){
-		this.server  = new CM(data);
+		this.server  = new ClientManager(data);
 		//this.clients = new ClientManager();
 		//this.server.event.on('open', (id,client,req) =>{});
 		this.state = 'init';
-		this.update(1000);
+		//this.update(1000);
 	} 
-	update(tick){
-
+	startUpdate(self){
+		self.update(1000);
 	}
-	//sequence
-
+	update(tick){
+		this.updateMatching();
+	}
 
 }
-var ms = new MS({server: HTTPserver(WS_PORT)});
+var ms = new MatchingServer({server: HTTPserver(WS_PORT)});
